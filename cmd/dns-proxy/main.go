bc15362d5b8050c5810d2d1343bd5c3ebde331ad
Vladimir Dulenov <originalrad50@gmail.com>
2024-03-05 21:50:41 +0100

    feat: main.go was updated

--- cmd/dns-proxy/main.go
+++ cmd/dns-proxy/main.go
@@ -22,27 +22,35 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
-	if *tcpMode {
-		c := &config.Config{}
-		c.Defaults()
+	c := &config.Config{}
+	c.Defaults()
 
-		if *configPath != "" {
-			c.ReadConfigFile(*configPath)
-		}
-		// initialize the logger
-		err := logger.InitLogger(c)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	if *configPath != "" {
+		c.ReadConfigFile(*configPath)
+	}
+	// initialize the logger
+	err := logger.InitLogger(c)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	// create a new DNS server in the specified mode
+	if *tcpMode {
 		// create a new server and listen on the TCP network address
 		dnsServer := dns.NewServer("tcp", c)
 		err = dnsServer.ListenAndServe()
 		if err != nil {
 			fmt.Println(err)
+			os.Exit(1)
 		}
 	}
 	if *udpMode {
 		fmt.Println("udp")
+		dnsServer := dns.NewServer("udp", c)
+		err = dnsServer.ListenAndServe()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
